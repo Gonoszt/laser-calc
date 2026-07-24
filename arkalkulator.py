737508b998bb7c192d723bf8b38fc51361440941
Gonoszt <gonosztorpe@gmail.com>
2026-07-24 17:48:00 +0200

    Update arkalkulator.py

--- arkalkulator.py
+++ arkalkulator.py
@@ -14,14 +14,21 @@ except ImportError:
 
 # --- ADATBÁZIS KAPCSOLAT (Firestore) ---
 def get_db_client():
-    if "gcp_json" in st.secrets:
+    if "project_id" in st.secrets:
         try:
-            sec_val = st.secrets["gcp_json"]
-            # Ha táblaként (dict-szerűen) érkezik a TOML-ből:
-            if hasattr(sec_val, "items"):
-                info = dict(sec_val)
-            else:
-                info = json.loads(sec_val)
+            info = {
+                "type": st.secrets["type"],
+                "project_id": st.secrets["project_id"],
+                "private_key_id": st.secrets["private_key_id"],
+                "private_key": st.secrets["private_key"].replace("\\n", "\n"),
+                "client_email": st.secrets["client_email"],
+                "client_id": st.secrets["client_id"],
+                "auth_uri": st.secrets["auth_uri"],
+                "token_uri": st.secrets["token_uri"],
+                "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+                "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+                "universe_domain": st.secrets.get("universe_domain", "googleapis.com")
+            }
 
             credentials = service_account.Credentials.from_service_account_info(info)
             return firestore.Client(credentials=credentials)
