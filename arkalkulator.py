0f8175b4cf9283a574f6c5b09468d4559d318193
Gonoszt <gonosztorpe@gmail.com>
2026-07-27 08:41:56 +0200

    Update arkalkulator.py

--- arkalkulator.py
+++ arkalkulator.py
@@ -6,33 +6,21 @@ from datetime import datetime
 from google.cloud import firestore
 from google.oauth2 import service_account
 
-# --- SVG elemzéshez szükséges könyvtár ---
-try:
-    from svgpathtools import svg2paths
-except ImportError:
-    st.error("Hiányzó csomag! A requirements.txt-be írd bele: svgpathtools")
+import streamlit as st
+import json
+from google.oauth2 import service_account
+from google.cloud import firestore
 
-# --- ADATBÁZIS KAPCSOLAT (Firestore) ---
+# --- ADATBÁZIS KAPCSOLAT ---
 def get_db_client():
-    if "project_id" in st.secrets:
+    if "gcp_json" in st.secrets:
         try:
-            # Megtisztítjuk a sorokat a felesleges szóközöktől
-            cleaned_lines = [line.strip() for line in st.secrets["private_key_lines"]]
-            full_private_key = "\n".join(cleaned_lines)
+            raw_json = st.secrets["gcp_json"]
+            info = json.loads(raw_json)
             
-            info = {
-                "type": st.secrets["type"],
-                "project_id": st.secrets["project_id"],
-                "private_key_id": st.secrets.get("project_id_key", "7f6de1835b9aba0b30298ee11e51aeb67665f198"),
-                "private_key": full_private_key,
-                "client_email": st.secrets["client_email"],
-                "client_id": st.secrets["client_id"],
-                "auth_uri": st.secrets["auth_uri"],
-                "token_uri": st.secrets["token_uri"],
-                "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-                "client_x509_cert_url": st.secrets["client_x509_cert_url"],
-                "universe_domain": st.secrets.get("universe_domain", "googleapis.com")
-            }
+            if "private_key" in info:
+                # Javítjuk a sortöréseket
+                info["private_key"] = info["private_key"].replace("\\n", "\n")
 
             credentials = service_account.Credentials.from_service_account_info(info)
             return firestore.Client(credentials=credentials)
