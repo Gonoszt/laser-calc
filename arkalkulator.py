91cb44f4b4090908478e282b24a11b47c31da0a9
Gonoszt <gonosztorpe@gmail.com>
2026-07-27 10:15:42 +0200

    arkalkulator.py

--- arkalkulator.py
+++ arkalkulator.py
@@ -14,18 +14,21 @@ except ImportError:
 
 # --- ADATBÁZIS KAPCSOLAT (Firestore) ---
 def get_db_client():
-    if "gcp_json" in st.secrets:
+    if "project_id" in st.secrets:
         try:
-            raw_json = st.secrets["gcp_json"]
-            if hasattr(raw_json, "items"):
-                info = dict(raw_json)
-            else:
-                info = json.loads(raw_json)
-            
-            if "private_key" in info:
-                # Itt történik a varázslat: a stringként kapott escaped sortöréseket igazi newline-ná alakítjuk
-                info["private_key"] = info["private_key"].replace("\\n", "\n")
-
+            info = {
+                "type": st.secrets["type"],
+                "project_id": st.secrets["project_id"],
+                "private_key_id": st.secrets["private_key_id"],
+                "private_key": st.secrets["private_key"],
+                "client_email": st.secrets["client_email"],
+                "client_id": st.secrets["client_id"],
+                "auth_uri": st.secrets["auth_uri"],
+                "token_uri": st.secrets["token_uri"],
+                "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+                "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+                "universe_domain": st.secrets.get("universe_domain", "googleapis.com")
+            }
             credentials = service_account.Credentials.from_service_account_info(info)
             return firestore.Client(credentials=credentials)
         except Exception as e:
